bad823b6c2fbcfe3599f0f517acabea1fbfad95b
Jacek Krzykwa <195395@student.pwr.edu.pl>
2016-07-04 17:16:49 +0200

    Adding new script

---
+++ Assets/BrokenWord/GitTestScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace #PROJECTNAME# {
+
+	/// <summary>
+	/// Write_class_description_here
+	/// </summary>
+	public class GitTestScript : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+	
+		}
+	
+		// Update is called once per frame
+		void Update () {
+	
+		}
+	}
+
+}
